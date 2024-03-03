b3eddb6d48ade835c6a8a9ab1dba11cac0b07823
DWL5 <ssop6403@gmail.com>
2024-03-04 01:39:01 +0900

    [spring-boot-intro] 스프링 DB 접근 기술 - 순수 JDBC

--- spring-boot-intro/src/main/java/hello/spring/SpringConfig.java
+++ spring-boot-intro/src/main/java/hello/spring/SpringConfig.java
@@ -1,14 +1,25 @@
 package hello.spring;
 
 import hello.spring.controller.MemberController;
+import hello.spring.repository.JdbcMemberRepository;
 import hello.spring.repository.MemberRepository;
 import hello.spring.repository.MemoryMemberRepository;
 import hello.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +28,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
---
+++ spring-boot-intro/src/main/java/hello/spring/repository/JdbcMemberRepository.java
@@ -0,0 +1,118 @@
+package hello.spring.repository;
+
+import hello.spring.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcMemberRepository implements MemberRepository {
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(name) values(?)";
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pstmt.setString(1, member.getName());
+            pstmt.executeUpdate();
+
+            try (ResultSet rs = pstmt.getGeneratedKeys()) {
+                if (rs.next()) {
+                    member.setId(rs.getLong(1));
+                } else {
+                    throw new SQLException("id 조회 실패");
+                }
+                return member;
+            }
+
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        String sql = "select * from member where id = ?";
+
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setLong(1, id);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    Member member = new Member();
+                    member.setId(rs.getLong("id"));
+                    member.setName(rs.getString("name"));
+                    return Optional.of(member);
+                } else {
+                    return Optional.empty();
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        String sql = "select * from member where name = ?";
+
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+
+            try (ResultSet rs = pstmt.executeQuery()){
+                if (rs.next()) {
+                    Member member = new Member();
+                    member.setId(rs.getLong("id"));
+                    member.setName(rs.getString("name"));
+                    return Optional.of(member);
+                }
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public List<Member> findAll() {
+        String sql = "select * from member";
+
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()){
+
+            List<Member> members = new ArrayList<>();
+            while (rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                members.add(member);
+            }
+            return members;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+}
