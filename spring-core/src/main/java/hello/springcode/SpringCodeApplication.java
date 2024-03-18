a58da248a700f486b2f8c5309d4c0a04ee9ba69e
DWL5 <ssop6403@gmail.com>
2024-03-19 00:33:47 +0900

    [spring-core] 스프링 핵심 원리 이해 1

---
+++ spring-core/src/main/java/hello/springcode/SpringCodeApplication.java
@@ -0,0 +1,13 @@
+package hello.springcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCodeApplication.class, args);
+    }
+
+}
---
+++ spring-core/src/test/java/hello/springcode/SpringCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
